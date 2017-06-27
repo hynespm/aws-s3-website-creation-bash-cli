5a3cd19d19f1772adfba3e27fc91e683f76903ea
hynespm <hynespm@gmail.com>
2017-06-27 17:27:09 +0200

    adjment of scripts

--- aws-create-s3-website.sh
+++ aws-create-s3-website.sh
@@ -6,25 +6,25 @@
 
 WEBSITE_NAME=$1
 REGION=$2
+PROFILE=$3
 
 # Create the two bucket names
 PROD_WEBSITE_NAME=$WEBSITE_NAME"-prod"
-STAG_WEBSITE_NAME=$WEBSITE_NAME"-stag"
+STAG_WEBSITE_NAME=$WEBSITE_NAME"-dev"
 
 echo "Creating S3 Buckets for Website Hosting ...."
 echo "S3 Bucket Name: " $WEBSITE_NAME
 
 echo "Creating staging website...."
-aws s3 mb s3://$STAG_WEBSITE_NAME --region $REGION
-aws s3 website s3://$STAG_WEBSITE_NAME/ --index-document index.html --error-document error.html
+aws s3 mb s3://$STAG_WEBSITE_NAME --region $REGION --profile $PROFILE
+aws s3 website s3://$STAG_WEBSITE_NAME/ --index-document index.html --error-document error.html --profile $PROFILE
 echo "Staging created..."
 
-echo "Creating production website....
-aws s3 mb s3://$PROD_WEBSITE_NAME --region $REGION
-aws s3 website s3://$PROD_WEBSITE_NAME/ --index-document index.html --error-document error.html
+echo "Creating production website...."
+aws s3 mb s3://$PROD_WEBSITE_NAME --region $REGION --profile $PROFILE
+aws s3 website s3://$PROD_WEBSITE_NAME/ --index-document index.html --error-document index.html --profile $PROFILE
+
 echo "Production created..."
-
-aws s3api put-bucket-policy --bucket $STAG_WEBSITE_NAME --policy file://policy-file/stag-policy.json
-aws s3api put-bucket-policy --bucket $PROD_WEBSITE_NAME --policy file://policy-file/prod-policy.json
-
-read -p "AWS S3 Static Website creation complete"
+aws s3api put-bucket-policy --bucket $STAG_WEBSITE_NAME --policy file://policy-file/stag-policy.json --profile $PROFILE
+aws s3api put-bucket-policy --bucket $PROD_WEBSITE_NAME --policy file://policy-file/prod-policy.json --profile $PROFILE
+echo "Policies applied..."
